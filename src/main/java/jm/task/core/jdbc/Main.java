e95beca581d95a6f8ef00c1bdf0fe65cc4e20e12
Sergei Potapov <Potapov1995@bk.ru>
2025-03-19 23:07:52 +0300

    second task complete

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Jon", "Shelby", (byte) 19);
+        userService.saveUser("Harry", "Li", (byte) 30);
+        userService.saveUser("Simon", "Strong", (byte) 22);
+        userService.saveUser("Sam", "Grey", (byte) 33);
+
+        userService.removeUserById(1);
+        System.out.println("Список пользователей:");
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
